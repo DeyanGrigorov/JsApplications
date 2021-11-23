fcf81ddb51b068e5e590a8a558f158c047e882f8
Deyan Grigorov <deian1998@abv.bg>
2021-11-24 00:23:07 +0200

    Add tests for BookLibrary

--- Architecture-And-Testing/02.Book-Library/test.js
+++ Architecture-And-Testing/02.Book-Library/test.js
@@ -2,15 +2,34 @@ const {chromium} = require('playwright-chromium');
 const {expect} = require('chai');
 
 
+const mockData = {"d953e5fb-a585-4d6b-92d3-ee90697398a0":
+{"author":"J.K.Rowling",
+"title":"Harry Potter and the Philosopher's Stone"},
+"d953e5fb-a585-4d6b-92d3-ee90697398a1":
+{"author":"Svetlin Nakov",
+"title":"C# Fundamentals"}
+}
+
+function json(data){
+  return {
+    status: 200,
+    headers:{
+      'Access-Control-Allow-Origin': '*',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }
+}
+
 
 
 describe('Tests', async function(){
-  this.timeout(5000);
+  this.timeout(6000);
 
   let page, browser;
 
   before(async () =>{
-    browser = await chromium.launch();
+    browser = await chromium.launch({headless: false, slowMo: 1000});
   });
 
   after(async () =>{
@@ -27,7 +46,12 @@ describe('Tests', async function(){
   });
 
   it('loads and displays all books', async () =>{
-    await page.goto('http://localhost:5500/02.Book-Library');
+    await page.route(`**/jsonstore/collections/books`,(route) =>{
+      route.fulfill(json(mockData))
+    })
+    
+
+    await page.goto('http://localhost:5500/Architecture-And-Testing/02.Book-Library/')
     
     await page.click('text=Load All Books');
 
@@ -41,9 +65,30 @@ describe('Tests', async function(){
     expect(rows[2]).to.contains('Nakov')
 
 
+  
+
+  });
+
+  it('can create book', async() =>{
+    await page.goto('http://localhost:5500/Architecture-And-Testing/02.Book-Library/');
+
+    await page.fill('form#createForm >> input[name="title"]', 'Title');
+    await page.fill('form#createForm >> input[name="author"]', 'Author');
+
+    const [request] = await Promise.all([
+      page.waitForRequest(request => request.method() == 'POST'),
+      page.click('form#createForm >> text=Submit')
+    ])
+    
+    await page.click('text=Load All Books');
+    
+    const data = JSON.parse(request.postData());
+    expect(data.title).to.equal('Title');
+    expect(data.author).to.equal('Author');
 
 
   });
+
 
 
 })
