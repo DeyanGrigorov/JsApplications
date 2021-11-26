97a4b5b22f87e0fdf37684c4b9b0cdcfb7fbeab2
Deyan Grigorov <deian1998@abv.bg>
2021-11-26 14:05:46 +0200

    Add new exc

---
+++ 04.Fill-Dropdown/dropdown.js
@@ -0,0 +1,49 @@
+import {html, render} from './node_modules/lit-html/lit-html.js'
+
+
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+
+const selectTemplate = (items) => html`
+<select id='menu'>
+    ${items.map(i => html`<option value=${i._id}>${i.text}</option>`)}
+</select>`;
+
+const root = document.querySelector('div');
+document.querySelector('form').addEventListener('submit', addItem);
+getData();
+
+
+async function getData(){
+    const res = await fetch(url);
+    const data = await res.json();
+    update(Object.values(data));
+}
+
+function update(items){
+    const result = selectTemplate(items)
+    render(result, root)
+}
+
+async function addItem(ev){
+    ev.preventDefault();
+    const text = document.getElementById('itemText').value;
+    if(text == ''){
+        return
+    }else{
+        const res = await fetch(url, {
+            method: 'post',
+            headers: {
+                'Content-type': 'application/json',
+    
+            },
+            body: JSON.stringify({text})
+        });
+        if (res.ok){
+            getData()
+            document.getElementById('itemText').value = ''
+        }
+    }
+
+}
+
